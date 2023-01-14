f30c90b270503612e49ab59cd7e7d9fa21e05990
Steve Fallon <steve.fallon@gmail.com>
2023-01-14 21:21:15 +0000

    u

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ ssl: {
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -48,51 +47,9 @@ app.use(cors())
 // Routes
 //=================================================================================================
 //
-//=================================================================================================
-// this is the /chess route 
-//=================================================================================================
-app.get('/chess', async (req, res) => {
-  console.log('/ route');
-  const client = await pool.connect();
-  //-------------------------------------------------------------------------------------
-  text = 'SELECT * FROM table01 WHERE id = $1'
-  values = ['1']
-  //-------------------------------------------------------------------------------------
-  try {
-  const result = await client.query(text,values);
-  const results = { 'results': (result) ? result.rows : null};
-  console.log("===> results: " + JSON.stringify(results));
-  console.log(results.results[0].id);
-  var result_id = (results.results[0].id);
-  res.render('pages/game' , results);
-  client.release();
-  } catch (err) {
- console.error('Error ' + err);
- res.send("Error " + err)
-  }
-})
-
-
-/*
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index'))
   
-  .get('/db', async (req, res) => {
+app.get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
@@ -105,5 +62,4 @@ express()
     }
   })
   
-  */
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
