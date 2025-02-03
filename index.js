2f4c3fd3cc7ab09ff18311a0379498b0fc32c517
Steve Fallon <steve.fallon@gmail.com>
2025-02-03 22:30:11 +0000

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('*      ========= secret-shore-85438 ========                       
 console.log('*      ========= postgresql-perpendicular-72828 using connection string ========     *');       
 console.log('*      ==== USES chess_table =====                                                   *');
 console.log('*                                                    xx        YY                    *');
-console.log('**************************************************************************************');
+console.log('*************************************************************************************');
 /*******/
 const express = require('express');
 const app = express();
